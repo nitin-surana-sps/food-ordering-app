cfbfa1f668a76e6bd9c141fd744608d602bd822e
Luying Jiang <ljiang@sps-program.com>
2022-07-09 11:55:28 +0000

    Set up new menu item blocks on the landing page using fake array data (script.js)

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,11 +1,13 @@
 // Restruant Information Edit Button
-
 let resButton = document.getElementById("restruantEdit");
 
 resButton.addEventListener("click", function() {
     // your code here...
-  window.location.replace("resturant.html");
-});
+  window.location.replace("");
+})
+
+
+
 
 // insert value to restruantName
 
@@ -22,3 +24,47 @@ AddButton.addEventListener("click", function() {
     // your code here...
   window.location.replace("menu.html");
 });
+
+
+// Menu Flow
+
+// Async get function to get the real data 
+
+
+// using fake data for now
+
+let fakeData = ["Spegatti",12, "Pasta", 13, "Fettuccine ", 14];
+
+let dataLenghth = fakeData.length;
+
+let id = 0,itemName = 0,price = 0;
+
+
+
+for (let i = 0; i < dataLenghth; i+=2) {
+  // set itemName and price
+  itemName = fakeData[i];
+  price = fakeData[i+1];
+  // append the created block to menu 
+  document.getElementById('menuBoxContainer').appendChild(newBlock(itemName,price));
+}
+
+
+//function that creates the child element 
+function newBlock(itemName,price){
+  let div = document.createElement('div');
+  div.innerHTML = `
+    <div class="pinkBox">
+        <div class="boxText">
+          <div id="itemID">Item ID </div>
+          <div class="child">${itemName}</div>
+          <div class="child">${price}</div>
+        </div>
+        <div class="boxButton">
+            <button class="editButton">Edit</button>
+        </div>
+    </div>
+`;
+  return div
+}
+
