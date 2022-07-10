db625f6c890e4acc7683868b8a7173734fd07ed8
Elwin He <50185565+elwinhe@users.noreply.github.com>
2022-07-10 07:26:58 -0400

    Create submit.js

---
+++ src/main/webapp/submit.js
@@ -0,0 +1,8 @@
+//Submit Button
+let resButton = document.getElementById("submitButton");
+
+//when clicked, redirects back to the index, plus calls datastore (to be added)
+resButton.addEventListener("click", function() {
+    // your code here...
+  window.location.replace("index.html");
+})
